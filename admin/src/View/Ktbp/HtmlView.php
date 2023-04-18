a508f75d2b97348b4e2a80f4b78945cd0ab43d1d
Gyurltrini <kathryn.ma@gmail.com>
2023-04-18 15:43:56 -0400

    Update HtmlView.php

--- admin/src/View/Ktbp/HtmlView.php
+++ admin/src/View/Ktbp/HtmlView.php
@@ -1,1 +1,33 @@
+<?php
+
+namespace KatanTechnologies\Component\KTBlankPage\Administrator\View\Ktbp;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_ktblankpage
+ *
+ * @copyright   Copyright (C) 2023 Kathryn Anderson. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Main "KT Blank Page" Admin View
+ */
+class HtmlView extends BaseHtmlView {
+    
+    /**
+     * Display the main "KT Blank Page" view
+     *
+     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     * @return  void
+     */
+    function display($tpl = null) {
+        parent::display($tpl);
+    }
+
+}
 
