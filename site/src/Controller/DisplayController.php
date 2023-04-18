64d173b436692faab737a67cb58c55c7b9024164
Gyurltrini <kathryn.ma@gmail.com>
2023-04-18 15:49:55 -0400

    Update DisplayController.php

--- site/src/Controller/DisplayController.php
+++ site/src/Controller/DisplayController.php
@@ -1,1 +1,37 @@
+<?php
+
+namespace KatanTechnologies\Component\KTBlankPage\Site\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Factory;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_ktblankpage
+ *
+ * @copyright   Copyright (C) 2023 Kathryn Anderson. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * KTBlankPage Component Controller
+ * @since  0.0.2
+ */
+class DisplayController extends BaseController {
+    
+    public function display($cachable = false, $urlparams = array()) {        
+        $document = Factory::getDocument();
+        $viewName = $this->input->getCmd('view', 'login');
+        $viewFormat = $document->getType();
+        
+        $view = $this->getView($viewName, $viewFormat);
+        $view->setModel($this->getModel('Message'), true);
+        
+        $view->document = $document;
+        $view->display();
+    }
+    
+}
 
