df4e3cfb94d6216ddd5f10d415275ea3817eb721
Gyurltrini <kathryn.ma@gmail.com>
2023-04-18 15:45:14 -0400

    Create default.php

---
+++ admin/tmpl/ktbp/default.php
@@ -0,0 +1,16 @@
+<?php
+
+use Joomla\CMS\Language\Text;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_ktblankpage
+ *
+ * @copyright   Copyright (C) 2023 Kathryn Anderson. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+ // No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<h2><?= Text::_('COM_KTBP_MSG_HELLO_WORLD') ?></h2>
