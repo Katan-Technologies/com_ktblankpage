ee90ca3dd9f69a190e95d3b5ee588650b4e274c3
Gyurltrini <kathryn.ma@gmail.com>
2023-04-18 15:41:09 -0400

    Update DisplayController.php

--- admin/src/Controller/DisplayController.php
+++ admin/src/Controller/DisplayController.php
@@ -1,1 +1,36 @@
+<?php
+
+namespace KatanTechnologies\Component\KTBlankPage\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_ktblankpage
+ *
+ * @copyright   Copyright (C) 2023 Kathryn Anderson. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Default Controller of KT Blank Page component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_ktblankpage
+ */
+class DisplayController extends BaseController {
+    /**
+     * The default view for the display method.
+     *
+     * @var string
+     */
+    protected $default_view = 'ktbp';
+    
+    public function display($cachable = false, $urlparams = array()) {
+        return parent::display($cachable, $urlparams);
+    }
+    
+}
 
