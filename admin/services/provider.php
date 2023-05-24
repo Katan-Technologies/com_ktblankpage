ca59d2a667aee4734be78a8b1c3a49332774d3ff
Gyurltrini <kathryn.ma@gmail.com>
2023-05-24 10:27:19 -0400

    Update provider.php

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -11,6 +11,14 @@ use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_ktblankpage
+ *
+ * @copyright   Copyright (C) 2023 Kathryn Anderson. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
 return new class implements ServiceProviderInterface {
     
     public function register(Container $container): void {
